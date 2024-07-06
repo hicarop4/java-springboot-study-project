d08bf0cf232f6350c05e0a8c8b20c9c6aa733b15
Hicaro V <hicaro.abreu@ufv.br>
2024-07-06 15:52:44 -0300

    feat: add security config, filter and details services

--- src/main/java/com/example/repositories/UserRepository.java
+++ src/main/java/com/example/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.repositories;
 import com.example.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
